ae8f5308951b726ed33bfcd9b7370905a202927c
Jose G. Basanta <jgj1989@gmail.com>
2024-02-27 13:15:08 -0400

    React code refactor

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const [quotes, setQuotes] = useState([]);
+  const [quoteText, setQuoteText] = useState("");
+  const [quoteAuthor, setQuoteAuthor] = useState("");
+
+  const getQuotes = async () => {
+    try {
+      const response = await axios.get("https://type.fit/api/quotes");
+      setQuotes(response.data);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const getQuote = () => {
+    if (quotes.length > 0) {
+      const randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+      setQuoteText(randomQuote.text);
+      setQuoteAuthor(randomQuote.author);
+    }
+  };
+
+  useEffect(() => {
+    getQuotes();
+  }, []);
+
+  useEffect(() => {
+    console.log(quotes);
+    getQuote();
+  }, [quotes]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="quote-box">
+      <p id="text">"{quoteText}"</p>
+      <p id="author">- Author: {quoteAuthor}</p>
+      <button id="new-quote" onClick={getQuote} type="button">
+        New Quote
+      </button>
+      <a id="tweet-quote">Tweet</a>
     </div>
   );
 }
