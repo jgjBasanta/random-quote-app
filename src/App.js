aa4ef0a971d2590bb5d15513dcc5873a1d47bc57
Jose G. Basanta <jgj1989@gmail.com>
2024-03-11 13:09:07 -0400

    CSS Grid template

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import React, { useState, useEffect, useCallback } from "react";
 
 function App() {
   const [quotes, setQuotes] = useState([]);
-  const [quoteText, setQuoteText] = useState("");
-  const [quoteAuthor, setQuoteAuthor] = useState("");
+  const [quoteText, setQuoteText] = useState("Loading a quote");
+  const [quoteAuthor, setQuoteAuthor] = useState("Anonymous");
+  const [initialLoad, setInitialLoad] = useState(true);
 
   const getQuotes = useCallback(async () => {
     try {
@@ -24,25 +25,36 @@ function App() {
     }
   };
 
+  let tweetText = `"${quoteText}" - ${quoteAuthor}`;
+
   useEffect(() => {
     getQuotes();
   }, []);
 
   useEffect(() => {
-    console.log(quotes);
-    getQuote();
-  }, [quotes]);
+    // Call getQuote immediately after loading quotes
+    if (initialLoad && quotes.length > 0) {
+      getQuote();
+      setInitialLoad(false);
+    }
+  }, [initialLoad, quotes]);
 
   return (
-    <div id="quote-box">
-      <p id="text">"{quoteText}"</p>
-      <p id="author">- Author: {quoteAuthor}</p>
-      <button id="new-quote" onClick={getQuote} type="button">
-        New Quote
-      </button>
-      <a id="tweet-quote" href="https://twitter.com/intent/tweet">
-        Tweet
-      </a>
+    <div id="background">
+      <div id="quote-box">
+        <p id="text">"{quoteText}"</p>
+        <p id="author">Author: {quoteAuthor}</p>
+        <button id="new-quote" onClick={getQuote} type="button">
+          New Quote
+        </button>
+        <a
+          id="tweet-quote"
+          href={`https://twitter.com/intent/tweet?text=${tweetText}`}
+          target="_blank"
+        >
+          Tweet Quote
+        </a>
+      </div>
     </div>
   );
 }
